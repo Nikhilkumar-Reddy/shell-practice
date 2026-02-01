2e64a42e9df8ab1d9d676fa268d855e7f9357674
Nikhil <nirhalnihar8344@gmail.com>
2026-02-02 00:21:29 +0530

    My first schell-scripting

---
+++ 01-hello-world.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+#The above command is called shebang, it is an interpreter of the code in shell scripting that check and compiles for errors
+
+# In shell scripting echo is used to print the statments. 
+echo "Hello Schell Scripting"
+
+#Once code was successfully return you would like to push the code to github right ? Currently your code in workplace.
+# from workplace - > stagging area/temp area (add) - > local repo ( commit) - > Git main/ remote (push) 
+# before you enter into staging area check the git status ( Its in Red Colour for the file)
+#Now add the code into staging area ( git add <filename> ) which confirms code in stagging area ( In greencolor file name)
+# this helps you to again move to unstage from green to red state (  git rm --cached <filename.sh> )
+# Now commit for loca repo ( git commit -m "you pushed the latest code")
+# still the code in local repo from here to remote origin (git push origin main)
+
+
